3fde9aca7a7b7085631e4c634035dc594554a70e
Hiarison Gigante <gigantedesousa@gmail.com>
2020-12-09 17:25:57 -0300

    update work profile for screenlayout

--- files/.screenlayout/work.sh
+++ files/.screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS1 --primary --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output VGA1 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output HDMI-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
