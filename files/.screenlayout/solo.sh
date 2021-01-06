ada335e83dc478531fb405f363e93e41f96d536a
Hiarison Gigante <gigantedesousa@gmail.com>
2021-01-05 23:08:54 -0300

    update solo screenlayout

--- files/.screenlayout/solo.sh
+++ files/.screenlayout/solo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output HDMI-1-1 --off
