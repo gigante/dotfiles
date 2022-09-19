d6c4d1b86c37a28565ffbedea1c784a9109c7a42
Hiarison Gigante <gigantedesousa@gmail.com>
2022-09-19 15:08:39 -0300

    fix: screenlayout

--- files/.screenlayout/work.sh
+++ files/.screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output HDMI-1-1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output DP2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
